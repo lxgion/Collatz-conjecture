f9d85389b61b46f2bedf9868852f441fbf92c163
lxgion <60397382+lxgion@users.noreply.github.com>
2023-02-02 12:23:58 +0100

    added intermediary cache
    huge optimization from v2 (26.97s) to 10.54s

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import os
+import sys
 import time
 
 def collatz(n, cache):
     steps = 0
+    seq = [n]
     while n != 1:
         if n in cache:
-            return steps + cache[n]
+            steps += cache[n][0]
+            seq = seq[:-1] + cache[n][1]
+            break
         if n % 2 == 0:
             n = n // 2
         else:
             n = 3 * n + 1
         steps += 1
+        seq.append(n)
+    cache[seq[0]] = (steps, seq)
     return steps
 
 def solve_collatz_conjecture(limit):
-    cache = {1: 0}
+    cache = {1: (0, [1])}
     max_steps = 0
     num = 0
     start_time = time.perf_counter()
@@ -24,14 +30,16 @@ def solve_collatz_conjecture(limit):
     estimated_time = 0
     for i in range(2, limit+1):
         steps = collatz(i, cache)
-        cache[i] = steps
         if steps > max_steps:
             max_steps = steps
             num = i
         elapsed_time = time.perf_counter() - start_time
         estimated_time = (1 - smoothing_factor) * estimated_time + smoothing_factor * elapsed_time * (limit / i)
-        print(f"Elapsed time: {elapsed_time:.2f}s. Estimated time remaining: {estimated_time - elapsed_time:.2f}s.", end="\r")
+        if i % sample_interval == 0:
+            print(f"Elapsed time: {elapsed_time:.2f}s. Estimated time remaining: {estimated_time - elapsed_time:.2f}s.", end="\r")
+            sys.stdout.flush()
     print()
+    print("Estimated time was", estimated_time)
     return num
 
 def clear_terminal():
